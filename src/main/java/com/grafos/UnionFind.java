8ddd121f1d3b6999cf75b10dbe5eef44abbef6d6
Keiver Esneid Castellanos Julio <02230131035@mail.udes.edu.co>
2026-04-16 22:33:57 -0500

    feat: Implementar UnionFind con path splitting y union by size para Kruskal

---
+++ src/main/java/com/grafos/UnionFind.java
@@ -0,0 +1,35 @@
+package com.grafos;
+
+public class UnionFind {
+    private final int[] parent;
+    private final int[] size;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) return;
+        if (size[rootX] < size[rootY]) {
+            parent[rootX] = rootY;
+            size[rootY] += size[rootX];
+        } else {
+            parent[rootY] = rootX;
+            size[rootX] += size[rootY];
+        }
+    }
+}
